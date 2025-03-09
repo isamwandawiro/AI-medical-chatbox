bdb1c6a69dd9d4d8216f34b128c4492b99e53fb8
Isabelah Mwandawiro <isabelahhadia@gmail.com>
2025-03-09 15:23:51 +0300

    added link to typewriter.js

--- src/index.js
+++ src/index.js
@@ -10,16 +10,31 @@ function displayResult(response) {
 }
 
 function getAnswer() {
+  let inputField = document.querySelector("#symptoms");
   let result = document.querySelector("#result");
+   if (!inputField.value.trim()) {
+     result.innerHTML = "Please enter your symptom(s)!";
+     return;
+   }
   result.innerHTML = "Generating answer, please wait a minute...";
 
   let apiKey = "103e0470750babf36f99e08ddo7f31ta";
   let context = "You are a medical doctor,provide precise and accurate information";
-  let prompt = "#symptom(s)";
+  let prompt = inputField.value;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}
   &context=${context}&key=${apiKey}`;
 
-  axios.get(apiUrl).then(displayResult);
+  axios.get(apiUrl).then(displayResult).catch((error) => {
+      result.innerHTML = "An error occurred. Please try again later.";
+      console.error(error);
+    });
 }
-let buttonElement = document.querySelector("button");
+let buttonElement = document.querySelector("input[type='button']");
 buttonElement.addEventListener("click", getAnswer);
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  alert("Welcome to AI medical chatbox 👋🏻");
+});
+
+
